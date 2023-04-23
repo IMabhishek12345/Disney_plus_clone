b6d4f3b8bce71c628c6c57396d498209f5e1c8e2
abhishek pokhriyal <abhishek.pokhriyal8@gmail.com>
2023-04-23 18:45:45 +0530

    Added a slider(carousel)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,37 @@
 import styled from "styled-components";
+import Imgslider from "./Imgslider";
+
 
 const Home=(props)=>{
+
  
      return (
-        <div>
-            Home
-        </div>
-     )
 
+      <Container>
+      <Imgslider />
+     </Container> 
+
+)
 }
+     
+ const Container=styled.main`
+ position: relative;
+ min-height: calc(100vh-250px);
+ overflow-x: hidden;
+ display: block;
+ top: 72px; 
+ padding:  calc(3.5vw + 5px );
+
+   &:after{
+    background: url("./images/home-background.png") center center / cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity:1;
+    z-index:-1;
+   }
+
+`;
+
 
 export default Home;
